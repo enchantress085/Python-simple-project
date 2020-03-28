95852e390daf1945eed5f2b98d563b234cfff696
enchantress <golammaulla085@gmail.com>
2020-03-28 21:54:03 +0600

    2nd update Movie-project.

--- movie_update .py
+++ movie_update .py
@@ -4,20 +4,9 @@
         'l' to see your movie,
         'f' to find a movie,
         'q' to quit:
-
-Tasks:
-[x]: Decide where to store movies
-[x]: What is the format of movie?
-[x]: Show the user the main interface and get their input
-[x]: Allow user to add movies 
-[]: Show all their movies 
-[]: Find a movie
-[x]: Stop running the program when they type 'q'          
-           
+            
 """
-
-movies = [] # --- store movies
-
+movies = []  # --- store movies
 """
 movie = {
     'name':...(string),
@@ -28,7 +17,8 @@ movie = {
 
 
 def menu():
-    user_input = input("Enter 'a' to add a movie, 'l' to see your movie, 'f' to find a movie, 'q' to quit :>")
+    user_input = input(
+        "Enter 'a' to add a movie, 'l' to see your movie, 'f' to find a movie, 'q' to quit :>")
 
     while user_input != 'q':
         if user_input == 'a':
@@ -39,47 +29,53 @@ def menu():
             find_movie(movies)
         else:
             print("Unknown commend-plese try again.")
-            
-        user_input = input("\nEnter 'a' to add a movie, 'l' to see your movie, 'f' to find a movie, 'q' to quit :> ")
-       
+
+        user_input = input(
+            "\nEnter 'a' to add a movie, 'l' to see your movie, 'f' to find a movie, 'q' to quit :> ")
+
+
 def add_movie():
     name = input("Enter the movie name:> ")
     director = input("Enter the movie director:> ")
     year = int(input("Enter the movie release year:> "))
-    
+
     movies.append({
-        'name' : name,
-        'director' : director,
-        'year' : year
+        'name': name,
+        'director': director,
+        'year': year
     })
+
 
 def show_movie(movie_list):
     for movie in movie_list:
         show_movie_details(movie)
+
 
 def show_movie_details(movie):
     print(f"Name: {movie['name']}")
     print(f"Director: {movie['director']}")
     print(f"Release Year :{movie['year']}")
 
+
 def find_movie():
-    find_by = input("What property of the movir looking for? ") #-- year , name, director
+    # -- year , name, director
+    find_by = input("What property of the movir looking for? ")
     looking_for = input("What are you searching for? ")
-    
+
     found_movies = find_by_attribute(movies, looking_for, lambda x: x[find_by])
-    
+
     show_movie(found_movies)
-    
-"""
 
-'anonymous function is a function that is defined without a name.
-lambda = lambda arguments: expression
 
 """
+############-------------
+
+"""
+
 
 def find_by_attribute(items, expected, finder):
     found = []
-    
+
     for i in items:
         if finder(i) == expected:
             found.append(i)
@@ -87,31 +83,4 @@ def find_by_attribute(items, expected, finder):
 
 
 menu()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# ------------
